acaa02ab108846d6cb46155e43e530e1ea50aad3
Viktor <viktor.m.rumenov@gmail.com>
2022-06-29 17:15:09 +0200

    Added static product element and functionality

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import React from "react";
+import Header from "./containers/Header";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ProductList from "./containers/ProductList";
+import ProductDetails from "./containers/ProductDetails";
 
 const App = () => {
-  return <div>Test</div>;
+  return (
+    <div>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<ProductList />} />
+          <Route path="/product/:productId" element={<ProductDetails />} />
+          <Route>404 Not Found</Route>
+        </Routes>
+      </Router>
+    </div>
+  );
 };
 
 export default App;
